3ee6cdd84c08eb8496d1019b4d1d3d54c1858b3e
Ayarde Christian <christianayarde@gmail.com>
2024-06-05 09:57:50 -0300

    Add new changes
    - Receiver side: * Add Throttle elapsed time function (see comments in LISP code).
    	         * Run rcode on a ESC device to send the joystick states
    
    - Remote side:   *Read buttons, throttle positions (js_x, js_y) and send to the receiver
    		 *Add elapsed time since a button is pressed (get_button_state).

--- Receiver_remote/Receiver_side.lisp
+++ Receiver_remote/Receiver_side.lisp
@@ -1,25 +1,38 @@
-;(import "pkg@://vesc_packages/lib_code_server/code_server.vescpkg" 'code-server)
-;(read-eval-program code-server)
-;(start-code-server)
+(import "pkg@://vesc_packages/lib_code_server/code_server.vescpkg" 'code-server)
+(read-eval-program code-server)
+
 (def can-id 13)
 (def other-peer '(255 255 255 255 255 255))
-(def mac-tx      '())
-(def set_cur     0.0)
-(def set_ana     0.0)
-(def vin         0.0)
-(def rpm         0.0)
-(def temp        0.0)
-(def speed       0.0)
-(def dist        0.0)
+(def mac-tx          '())
+(def set_cur         0.0)
+(def set_ana         0.0)
+(def vin             0.0)
+(def rpm             0.0)
+(def temp            0.0)
+(def speed           0.0)
+(def dist            0.0)
+(def button_state    0.0)
+(def enable_throttle 0.0)
+(def time            0.0)
+(def secs            0.0)
+(def last_time       0.0)
+(to-float secs)
 
 (esp-now-start)
 
-;(esp-now-add-peer other-peer) 
+(esp-now-add-peer other-peer) 
 
 (defun data-received (data) {
-        
-     (setq set_cur (bufget-f32 data 0 'little-endian))  ; analog channels from remote
-     (setq set_ana (bufget-f32 data 5 'little-endian))  ; 
+     
+     (setq secs (secs-since time ))
+       
+     (setq set_cur      (bufget-f32 data 0      'little-endian)) ; analog channels from remote
+     (setq set_ana      (bufget-f32 data 4      'little-endian)) ; 
+     (setq button_state (bufget-f32 data 8      'little-endian)) ; get the thumbstick button, config button states  
+     
+     (throttle_elapsed_time 5.0)
+    
+     (rcode-run-noret can-id (list 'set-remote-state set_cur 0 0 0 0))
      (free data)  
   }
 )
@@ -33,13 +46,32 @@
       (setq temp  (canget-temp-fet can-id))   
       (setq speed (canget-speed can-id))
       (setq dist  (canget-dist can-id))   
-      
-      (bufset-i16 data 0 rpm)
-      (bufset-f32 data 2 vin)
-      (bufset-f32 data 6 temp)
+   
+      (bufset-i16 data 0  rpm)
+      (bufset-f32 data 2  vin)
+      (bufset-f32 data 6  temp)
       (bufset-i16 data 10 speed)
-      
+      (bufset-i8  data 11 enable_throttle)
       (esp-now-send mac-tx data)
+  }
+)
+
+;Throttle is enabled when thumbstick button is pressed. If throttle is not applied 
+;within a time defined by "throttle_secs", it will be disabled. The current command 
+;"set_cur" is set to 0.
+
+(defun throttle_elapsed_time (throttle_secs) {
+        
+  (if (and (< button_state 1.8)(> button_state 1.6)) { ; if thumbstick button is pressed enables the throttle
+       (setq enable_throttle 1.0)
+      })
+    (if (< enable_throttle 1.0) {(setq set_cur 0.0)(setq time (secs-since secs))})
+    
+    (setq last_time (- secs time))
+    
+  (if (or (> set_ana 1.85)(< set_ana 1.6)) {(setq last_time 0.0)(setq time (secs-since secs))}                         
+         {(if (>= last_time throttle_secs) {(setq set_cur 0.0)(setq enable_throttle 0.0)}
+     }))                           
   }
 )
 
@@ -48,8 +80,7 @@
      (setq mac-tx src)
      (data-received data)
      (esp-now-add-peer mac-tx)
-     (canset-current-rel can-id set_cur)  
-})
+   })
                                                                 
 (defun event-handler ()
     (loopwhile t
--- Receiver_remote/Remote_side.lisp
+++ Receiver_remote/Remote_side.lisp
@@ -10,8 +10,16 @@
 (def temp         0.0)
 (def speed        0.0)
 (def dist         0.0)
-(def setpoint     0.0)
-(def analog_input 0.0)
+(def js_x         0.0); x axis
+(def js_y         0.0); y axis
+(def button_state 0.0); button config, thumbstick button
+(def enable_throttle 0.0)
+(def secs 0.0)
+(def time 0.0)
+(def last_update 0.0)
+(def button_time 2.0) ; time for button pressed
+
+(to-float secs)
 
 (esp-now-add-peer other-peer)
 
@@ -20,10 +28,11 @@
 )
 
 (defun data_received (data) {
-    (setq rpm    (bufget-i16 data 0))
-    (setq vin    (bufget-f32 data 2))
-    (setq temp   (bufget-f32 data 6))
-    (setq speed  (bufget-i16 data 10))    
+    (setq rpm             (bufget-i16 data 0))
+    (setq vin             (bufget-f32 data 2))
+    (setq temp            (bufget-f32 data 6))
+    (setq speed           (bufget-i16 data 10))
+    (setq enable_throttle (bufget-i8  data 11))    
     (free data)
  }
 )
@@ -32,16 +41,35 @@
 ;buttons input state to be added
 
 (defun data_to_send (data) {
-     (setq setpoint     (get-adc 2))
-     (setq analog_input (get-adc 0))
-     (def set_mapped (utils_map setpoint 0.67 2.73 0.0 -0.3))
-     (setq cur_set set_mapped)
+     (setq js_y           (get-adc 0))
+     (setq js_x           (get-adc 1))
+     (setq button_state   (get-adc 2))
+     
+     (setq cur_set (utils_map js_y 1.72 2.96 0.0 0.3))
+     
      (bufset-f32 data 0 cur_set      'little-endian); send a command current for the receiver
-     (bufset-f32 data 5 analog_input 'little-endian)
-    ;(esp-now-send other-peer data)
+     (bufset-f32 data 4 js_y         'little-endian); send "y" position
+     (bufset-f32 data 8 button_state 'little-endian); send throttle button, button config states
      (esp-now-send mac-rx data)
   }
 )
+; 
+
+(defun get_button_state () {  
+    (if(< button_state 1.3) {
+        (setq last_update (- secs time))
+        (if(>= last_update button_time ) {
+           (print "task"); do a task  
+              }
+            ) 
+          } 
+     {
+        (setq time (secs-since secs))
+       ; (print time)      
+       }
+     )
+   }
+ )
     
 (defun proc-data (src des data rssi) {
     (print (list "src:" src  "des:" des "data:" data "rssi:" rssi))
@@ -57,7 +85,6 @@
            ((event-esp-now-rx (? src) (? des) (? data) (? rssi)) (proc-data src des data rssi))
            (_ nil)
 )))
-
   
 (defun main () {
     (loop-param)
@@ -70,8 +97,10 @@
       (loopwhile-thd 50 t { 
             (var data (bufcreate 40)) 
             (data_to_send data)
+            (setq secs (secs-since time ))
+            (get_button_state)
             (free data)
-            (sleep 0.1)
+            (sleep 0.02)
    }
   )  
  }   
