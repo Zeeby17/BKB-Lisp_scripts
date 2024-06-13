91f46d37c6c3b38a15e56a9afa43335699d1b77c
Diego Sarlinga <dsarlinga@paltatech.com>
2024-06-13 14:53:17 -0300

    Modifie throttle time out algorithm
    - Prevent throttle to work outside the main menu
    - Reset time out when comming out from menu in such way that the thumb button must be pressed again to engage throttle

--- display/res/throttle.lisp
+++ display/res/throttle.lisp
@@ -1,30 +1,46 @@
-(def throttle_feed_time 0.0)
+
+(def last_update_throttle 0.0)
+(def throttle_time_out 0.0)
 (def throttle_status 0) ; 0 for inhibited 1 for active
 (def throttle 0.0)
 (def joy_Y 0.0)
 (def joy_min 0)
 (def joy_mid 0)
 (def joy_max 0)
+
 (defun throttle_th(){
-    (loopwhile t{   
-        (if (= thum_pressed_short 1){
+    (loopwhile t{
+
+        (setq joy_min (eeprom-read-i min_cal_add))
+        (setq joy_mid (eeprom-read-i mid_cal_add))
+        (setq joy_max (eeprom-read-i max_cal_add))
+        (if (= menu_index 0) ;don't read the throttle if not in the main screean
+            (setq joy_Y (get-adc-raw))
+            (setq joy_Y joy_mid)
+        )
+
+        (if (and (= thum_pressed_short 1) (= menu_index 0)){
             (setq thum_pressed_short 0)
-            (setq throttle_feed_time (systime));feed timeout
+            (setq throttle_time_out THR_TIMEOUT);feed timeout
         })
-        (if (and (= throttle_status 1) (> (get-adc-raw) (+ (eeprom-read-i mid_cal_add) 100))){
-            (setq throttle_feed_time (systime));feed timeout
+        (if (and (= throttle_status 1) (> joy_Y (+ joy_mid 100))){
+            (setq throttle_time_out THR_TIMEOUT);feed timeout
         })
-        (if(> (secs-since throttle_feed_time) THR_TIMEOUT){
+        
+        (setq throttle_time_out (- throttle_time_out (secs-since last_update_throttle)))
+        (setq last_update_throttle (systime))
+
+        (if (not-eq menu_index 0)
+            (setq throttle_time_out 0)
+        )
+
+        (if(<= throttle_time_out 0){
             (setq throttle_status 0) ; disable
         }
         {
             (setq throttle_status 1) ; enable
         })
         
-        (setq joy_Y (get-adc-raw))
-        (setq joy_min (eeprom-read-i min_cal_add))
-        (setq joy_mid (eeprom-read-i mid_cal_add))
-        (setq joy_max (eeprom-read-i max_cal_add))
         (if(= throttle_status 1){
             (if (> joy_Y joy_mid)
                 (setq throttle (utils_map joy_Y joy_mid joy_max 0.0 1.0))
@@ -32,7 +48,7 @@
             )
         }
         {
-            (if (< joy_Y joy_mid)
+            (if (<= joy_Y joy_mid)
                 (setq throttle (* (utils_map joy_Y joy_mid joy_min 0.0 1.0) -1))
             )              
         })
